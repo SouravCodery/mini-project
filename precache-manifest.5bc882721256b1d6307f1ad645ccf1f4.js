e6a7256ad7bbddd302f6c262f3682f630e4c93a1
Sourav Choudhary <48824431+SouravCodery@users.noreply.github.com>
2020-10-19 23:28:01 +0530

    Updates

---
+++ precache-manifest.5bc882721256b1d6307f1ad645ccf1f4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1350a82e85b778abaf2bebc587831e97",
+    "url": "/mini-project/index.html"
+  },
+  {
+    "revision": "337b221cad78a68ca05b",
+    "url": "/mini-project/static/css/main.ff550aca.chunk.css"
+  },
+  {
+    "revision": "959d09e19ddd793837eb",
+    "url": "/mini-project/static/js/2.b1937b53.chunk.js"
+  },
+  {
+    "revision": "3b6069e01f0db714f9e787b4dbe9775c",
+    "url": "/mini-project/static/js/2.b1937b53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337b221cad78a68ca05b",
+    "url": "/mini-project/static/js/main.625ad09d.chunk.js"
+  },
+  {
+    "revision": "333683d3f4e424b47b4b",
+    "url": "/mini-project/static/js/runtime-main.72767c26.js"
+  },
+  {
+    "revision": "8bc5c323c04c1187100a5f18cd934968",
+    "url": "/mini-project/static/media/anders-jilden-uwbajDCODj4-unsplash.8bc5c323.jpg"
+  },
+  {
+    "revision": "dec6e4b37c207d9d1285166af21b96e8",
+    "url": "/mini-project/static/media/benjamin-bousquet-bli6Z7xsPGE-unsplash.dec6e4b3.jpg"
+  },
+  {
+    "revision": "109af2c84387324ddafa885dfb14aea6",
+    "url": "/mini-project/static/media/h-heyerlein-riYdn15o96U-unsplash.109af2c8.jpg"
+  },
+  {
+    "revision": "1aafb7c4e5fc092a5648105764a6a85b",
+    "url": "/mini-project/static/media/jr-korpa-HvbspNAckOA-unsplash.1aafb7c4.jpg"
+  },
+  {
+    "revision": "7450677b70e1d4762e2f693373f2a523",
+    "url": "/mini-project/static/media/neonbrand-MENIBa0eZeA-unsplash.7450677b.jpg"
+  },
+  {
+    "revision": "1ab0bfe6c99924189dcd8928e08ca09b",
+    "url": "/mini-project/static/media/rhondak-native-florida-folk-artist-_Yc7OtfFn-0-unsplash.1ab0bfe6.jpg"
+  },
+  {
+    "revision": "6e21f3aa04670d27faf947622996aed4",
+    "url": "/mini-project/static/media/sharon-pittaway-iMdsjoiftZo-unsplash.6e21f3aa.jpg"
+  }
+]);
